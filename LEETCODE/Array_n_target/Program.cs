7c89a068318a7607bcc585a406c199109c69a02f
Vitaly <vitaly.rybkin@gmail.com>
2022-12-02 23:51:25 +0300

    new arrivals

---
+++ LEETCODE/Array_n_target/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+*/
+
+Console.Clear();
+
+int[] nums = { 2,5,5,11 };
+int target = 10;
+int[] target_arr = new int[2];
+bool targeted = false;
+for (int i = 0; i < nums.Length; i++) {
+    for (int j = i + 1; j < nums.Length; j++)
+    {
+        if (nums[i] + nums[j] == target) {
+            target_arr[0] = i;
+            target_arr[1] = j;
+            targeted = true;
+            break;
+        }
+    if (targeted) break;
+    }
+if (targeted) break;
+}
+
+foreach (int item in target_arr) Console.Write(item +" ");
+
---
+++ LEETCODE/Ransomnote_n_mag/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+Each letter in magazine can only be used once in ransomNote.
+*/
+
+using System.Text;
+
+Console.Clear();
+
+Console.Write("Enter a string RANSOMNOTE: " );
+string ransomNote = Console.ReadLine() ?? String.Empty;
+Console.Write("Enter a string MAGAZINE: " );
+string magazine = Console.ReadLine() ?? String.Empty;
+List<char> chars_from_ran = new List<char>(ransomNote);
+List<char> chars_from_mag = new List<char>(magazine);
+
+bool is_constructed = false;
+if (chars_from_ran.Count < chars_from_mag.Count) {
+    while (chars_from_ran.Count != 0) {
+        is_constructed = false;
+        for (int j = 0; j < chars_from_mag.Count; j++){
+            //if (chars_from_ran.Count == 0) break;
+            if (chars_from_ran[0] == chars_from_mag[j] && chars_from_ran.Count != 0) {
+                chars_from_ran.RemoveAt(0);
+                chars_from_mag.RemoveAt(j);
+                is_constructed = true;
+            }
+            if (is_constructed || chars_from_ran.Count == 0) break;
+        }
+        if (is_constructed == false) break;
+    }
+if (is_constructed) Console.WriteLine($"RansomNote \"{ransomNote}\" is constructed from magazine \"{magazine}\"!");
+else Console.WriteLine($"RansomNote \"{ransomNote}\" can't be constructed from magazine \"{magazine}\" !");
+}
+else Console.WriteLine($"Magazine \"{magazine}\" is shorter, than ransomNote \"{ransomNote}\""!);
+
+
+foreach (char item in chars_from_ran) Console.Write(item + " ");
+Console.WriteLine();
+foreach (char item in chars_from_mag) Console.Write(item + " ");
